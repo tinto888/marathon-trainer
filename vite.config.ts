c6713acbb147018c18d91296fad2fc3f83e0b359
tinto888 <hivoice88@gmail.com>
2026-05-03 16:42:42 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: "./", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
     },
   },
   build: {
-    outDir: 'dist',
-  }
+    outDir: "dist",
+  },
 });
